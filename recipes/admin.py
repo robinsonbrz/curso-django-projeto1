c38d65bb1e63ec02f29898bf6beaf1cb71c4df6a
ROBINSON ENEDINO E SILVA <robinsonbrz@gmail.com>
2022-02-11 23:58:51 -0300

    2022-02-11

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
+from .models import Category, Recipe
+
 # Register your models here.
+
+
+# Register your models here.
+# Registrando aqui é disponibilizado na area administrativa do Django
+class CategoryAdmin(admin.ModelAdmin):
+    ...
+
+
+# Registrando com decorator
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    ...
+
+
+# pode-se registrar com decorator:
+# @admin.register()
+admin.site.register(Category, CategoryAdmin)
