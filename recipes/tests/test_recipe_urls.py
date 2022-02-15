8a5bddb3854fb41c8ead638450572cb3daafb037
ROBINSON ENEDINO E SILVA <robinsonbrz@gmail.com>
2022-02-15 16:33:38 -0300

    2022-02-15 Search implemented with tests

--- recipes/tests/test_recipe_urls.py
+++ recipes/tests/test_recipe_urls.py
@@ -15,3 +15,7 @@ class RecipeURLsTest(TestCase):
     def test_recipe_detail_url_is_correct(self):
         home_url = reverse('recipes:recipe', kwargs={'id': 1})
         self.assertEqual(home_url, '/recipes/1/')
+
+    def test_recipe_search_url_is_correct(self):
+        home_url = reverse('recipes:search')
+        self.assertEqual(home_url, '/recipes/search/')
--- recipes/views.py
+++ recipes/views.py
@@ -1,4 +1,5 @@
-# from django.http import Http404
+from django.db.models import Q
+from django.http import Http404
 from django.shortcuts import get_list_or_404, get_object_or_404, render
 
 from recipes.models import Recipe
@@ -42,4 +43,29 @@ def recipe(request, id):
     return render(request, 'recipes/pages/recipe-view.html', context={
         'recipe': recipe,
         'is_detail_page': True,
+    })
+
+
+def search(request):
+    search_term = request.GET.get('q', '').strip()
+
+    if not search_term:
+        raise Http404()
+
+    recipes = Recipe.objects.filter(
+        # se fizer a busca apenas por title:
+        # precisa ser otítulo completo como foi digitado
+        # __icontains case insensitive e que contém o texto
+        # o pipe representa operador or
+        Q(
+            Q(title__icontains=search_term) |
+            Q(description__icontains=search_term),
+        ),
+        is_published=True
+    ).order_by('-id')
+
+    return render(request, 'recipes/pages/search.html', {
+        'page_title': f'Search for "{search_term}" |',
+        'search_term': search_term,
+        'recipes': recipes,
     })
