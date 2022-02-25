7aa03bf7cb0d68b5f36645e48ce8e542aaa62030
ROBINSON ENEDINO E SILVA <robinsonbrz@gmail.com>
2022-02-25 18:13:44 -0300

    2022-02-25 cbv tests  16 Last

--- recipes/tests/test_recipe_urls.py
+++ recipes/tests/test_recipe_urls.py
@@ -2,20 +2,21 @@ from django.test import TestCase
 from django.urls import reverse
 
 
-# Create your tests here.
 class RecipeURLsTest(TestCase):
     def test_recipe_home_url_is_correct(self):
-        home_url = reverse('recipes:home')
-        self.assertEqual(home_url, '/')
+        url = reverse('recipes:home')
+        self.assertEqual(url, '/')
 
     def test_recipe_category_url_is_correct(self):
-        home_url = reverse('recipes:category', kwargs={'category_id': 1})
-        self.assertEqual(home_url, '/recipes/category/1/')
+        url = reverse('recipes:category', kwargs={'category_id': 1})
+        self.assertEqual(url, '/recipes/category/1/')
 
     def test_recipe_detail_url_is_correct(self):
-        home_url = reverse('recipes:recipe', kwargs={'id': 1})
-        self.assertEqual(home_url, '/recipes/1/')
+        url = reverse('recipes:recipe', kwargs={'pk': 1})
+        self.assertEqual(url, '/recipes/1/')
 
     def test_recipe_search_url_is_correct(self):
-        home_url = reverse('recipes:search')
-        self.assertEqual(home_url, '/recipes/search/')
+        url = reverse('recipes:search')
+        self.assertEqual(url, '/recipes/search/')
+
+# RED - GREEN - REFACTOR
